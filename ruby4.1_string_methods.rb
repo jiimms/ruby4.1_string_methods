a11c8d39f032f4a72521a621e49b63868285b65e
jiimms <jiimms@yahoo.com>
2016-03-11 16:27:51 +0530

    strng methods

---
+++ ruby4.1_string_methods.rb
@@ -0,0 +1,19 @@
+
+	def stringMethods()
+		
+		message = "Welcome"
+		name = "Ruby"
+
+		puts "#{message}" + " " + "#{name}"
+
+		puts message + " "+ name.upcase	
+		puts message.downcase + name.downcase
+		puts name.reverse + " "+ message	
+		puts "#{message}".upcase + " " + "#{name}".upcase
+
+	end
+
+	stringMethods()
+	
+	
+		
